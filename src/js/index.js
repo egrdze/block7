c5a756d6569888156b0c1bf778be4f43a5050210
Egor <finnloljakeo@gmail.com>
2025-02-25 12:52:24 +0300

    Fix swiper-card

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,66 @@
 import { Grid } from 'swiper/modules'
 import '../scss/style.scss'
+import Swiper from 'swiper'
 
-// Пример инициализации нескольких слайдеров
-document.querySelectorAll('.swiper-container').forEach((container) => {
-  new Swiper(container, {
-    spaceBetween: 4,
-    swiperOptions: { direction: 'horizontal' },
-    breakpoints: {
-      320: {
-        slidesPerView: 1
-      },
-      768: {
-        grid: {
-          rows: 3,
-          fill: 'row'
-        }
-      },
-      1440: {
-        slidesPerView: 4,
-        grid: {
-          rows: 3,
-          fill: 'row'
-        }
-      }
+const swiper1 = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  spaceBetween: 4,
+  modules: [Grid],
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: {
+      grid: { rows: 3, fill: 'row' }
+    },
+    1440: {
+      slidesPerView: 4,
+      grid: { rows: 3, fill: 'row' }
     }
-  })
-
-  // Слайдер
-
-  // Боковое меню
-  const burgerMenu = document.querySelector('.header__burger__menu')
-  const sidebar = document.querySelector('.sidebar')
-  let isSidebarOpen = false
-
-  function toggleSidebar() {
-    sidebar.style.left = isSidebarOpen ? '-100%' : '0'
-    isSidebarOpen = !isSidebarOpen
   }
+})
 
-  // Открытие и закрытие бокового меню
-  if (burgerMenu) {
-    burgerMenu.addEventListener('click', function (event) {
-      event.stopPropagation()
-      toggleSidebar()
-    })
+const swiper2 = new Swiper('.swiper-container-box', {
+  wrapperClass: 'swiper-wrapper-box',
+  slideClass: 'swiper-slide-box',
+
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
   }
+})
 
-  document.addEventListener('click', function (event) {
-    if (
-      isSidebarOpen &&
-      !sidebar.contains(event.target) &&
-      event.target !== burgerMenu
-    ) {
-      toggleSidebar()
-    }
+// Боковое меню
+const burgerMenu = document.querySelector('.header__burger__menu')
+const sidebar = document.querySelector('.sidebar')
+let isSidebarOpen = false
+
+function toggleSidebar() {
+  sidebar.style.left = isSidebarOpen ? '-100%' : '0'
+  isSidebarOpen = !isSidebarOpen
+}
+
+// Открытие и закрытие бокового меню
+if (burgerMenu) {
+  burgerMenu.addEventListener('click', function (event) {
+    event.stopPropagation()
+    toggleSidebar()
   })
+}
+
+document.addEventListener('click', function (event) {
+  if (
+    isSidebarOpen &&
+    !sidebar.contains(event.target) &&
+    event.target !== burgerMenu
+  ) {
+    toggleSidebar()
+  }
 
   // Модальные окна
   const modalOverlayFeedback = document.getElementById('modalOverlayFeedback')
